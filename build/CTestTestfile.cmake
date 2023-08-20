1a2f65c44c5aa3184fff492813687fd85a478aef
giang <ddhduygiang@gmai.com>
2023-08-20 22:44:03 +0700

    learn urdf exercise 2.1

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ruyzang/urdf_learn/src
+# Build directory: /home/ruyzang/urdf_learn/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("my_mira_description")
---
+++ build/my_mira_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ruyzang/urdf_learn/src/my_mira_description
+# Build directory: /home/ruyzang/urdf_learn/build/my_mira_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
